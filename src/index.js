1e737193e6e8522f5507185ff436b88eff6d0390
Jeremey Hansen <jeremeyhansen@yahoo.com>
2022-08-12 15:47:49 -0400

    commit tree db0fad518bf7d02874b1d6e6d7cef78fe2d9a139

--- src/index.js
+++ src/index.js
@@ -1,5 +1,45 @@
-const init = () => {
-  
-}
+// const init = () => {
+//     //target the DOM element we want with the query tool 
+//     const inputForm = document.querySelector('form');
+//     //adding an event listener with the (inputForm) in our code
+//     //submit is the type of event, and then event is stored as a variable with a callback function 
+//     inputForm.addEventListener('submit', (event) => {
+//         //stops the page from defaulting back to the norm
+//         event.preventDefault();
+//         //gets the value from our event object by accessing the event, then the the property(children), then the second element via its index, and finally to get the value with the value attribute
+//         //event.target.children[1].value (the method below is the same thing)
+//         const input = document.querySelector('input#searchByID');
 
-document.addEventListener('DOMContentLoaded', init);
+//         console.log(input.value);
+
+//         fetch('http://localhost:3000/movies')
+//         .then(response => response.json())
+//         .then(data => {
+//           console.log(data);
+
+
+//     }));
+// }
+
+// document.addEventListener('DOMContentLoaded', init);
+
+const init = () => {
+    const inputForm = document.querySelector('form');
+  
+    inputForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      const input = document.querySelector('input#searchByID');
+    
+      fetch('http://localhost:3000/movies')
+      .then(response => response.json())
+      .then(data => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p');
+
+        title.innerText = data.title;
+        summary.innerText = data.summary;
+      });
+    });
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
